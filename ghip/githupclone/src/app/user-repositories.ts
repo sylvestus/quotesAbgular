7743111848f1a3b8819b06f6ecefa8f37d6277c4
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-14 16:41:06 +0300

    add our user repo class

---
+++ ghip/githupclone/src/app/user-repositories.ts
@@ -0,0 +1,10 @@
+export class UserRepositories {
+    constructor(
+        public html_url:string,
+        public name:string,
+        public description:string,
+        public language:string
+        
+        
+            ){}
+}
