2f7bdad185f7908c629b686d8fcc1f9f0fb0397e
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-14 11:16:56 +0300

    add our user repositories

---
+++ angular/githubCloneIp/ghclone/src/app/user-repositories/user-repositories.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-repositories',
+  templateUrl: './user-repositories.component.html',
+  styleUrls: ['./user-repositories.component.css']
+})
+export class UserRepositoriesComponent implements OnInit {
+
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
