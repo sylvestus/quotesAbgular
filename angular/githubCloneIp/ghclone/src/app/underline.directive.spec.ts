00a5ad8ad9a1ae3191b56dcb1a9897d3b0a317f7
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-14 13:39:02 +0300

    Remove duplicated directory

--- angular/githubCloneIp/ghclone/src/app/underline.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UnderlineDirective } from './underline.directive';
-
-describe('UnderlineDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UnderlineDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- angular/githubCloneIp/ghclone/src/app/user-repositories.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserRepositories } from './user-repositories';
-
-describe('UserRepositories', () => {
-  it('should create an instance', () => {
-    expect(new UserRepositories()).toBeTruthy();
-  });
-});
