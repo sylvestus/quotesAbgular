13a3aff113abedfc7196713d3b176482345761fe
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-14 16:42:07 +0300

    add our user repo class test files

---
+++ ghip/githupclone/src/app/user-repositories.spec.ts
@@ -0,0 +1,7 @@
+import { UserRepositories } from './user-repositories';
+
+describe('UserRepositories', () => {
+  it('should create an instance', () => {
+    expect(new UserRepositories()).toBeTruthy();
+  });
+});
